9c45b89a7b1bf08afcb4bfbba7fcf8e19af1eda4
unknown <joaquimmigueldecristo@gmail.com>
2025-10-08 19:55:24 +0200

    14

--- assets/js/view/eventos/home.js
+++ assets/js/view/eventos/home.js
@@ -6,5 +6,47 @@ export default {
             title: "Contato"
         }
     },
+
+    
+  async mounted() {
+
+
+ 
+    var swiper = new Swiper(".mySwiper", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+ 
+
+
+    var swiper = new Swiper(".slider_flayer", {
+      effect: "coverflow",
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: "auto",
+      coverflowEffect: {
+        rotate: 50,
+        stretch: 0,
+        depth: 100,
+        modifier: 1,
+        slideShadows: true,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
+  },
     template: await get_template('./assets/js/view/eventos/home')
 }
--- assets/js/view/home/home.js
+++ assets/js/view/home/home.js
@@ -14,7 +14,22 @@ export default {
 
   async mounted() {
 
-
+  var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
 
     AOS.init({
       duration: 1000,
